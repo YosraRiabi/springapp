de4ccb8404b3b2b73a260fc4c33458c5e724c024
YosraRiabi <riabiyosraa@gmail.com>
2019-01-16 13:46:30 +0100

    Adding mappings from Link -> Comment

--- src/main/java/com/riabi/springapp/domain/Comment.java
+++ src/main/java/com/riabi/springapp/domain/Comment.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 @NoArgsConstructor/*
@@ -25,6 +26,9 @@ public class Comment {
     private String body;
 
     //Link
+
+    @ManyToOne
+    private Link link;
 
 
 
--- src/main/java/com/riabi/springapp/domain/Link.java
+++ src/main/java/com/riabi/springapp/domain/Link.java
@@ -7,6 +7,9 @@ import lombok.NonNull;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -26,6 +29,9 @@ public class Link {
     private String url;
 
     //Comment
+
+    @OneToMany(mappedBy = "link")
+    private List<Comment> comments = new ArrayList<>();
 
 
 }
