7e55eaae76406b819b61a72cb91ae8a071ea6816
YosraRiabi <riabiyosraa@gmail.com>
2019-01-21 13:31:54 +0100

    Adding some changes to SpringAppApplication

--- src/main/java/com/riabi/springapp/SpringAppApplication.java
+++ src/main/java/com/riabi/springapp/SpringAppApplication.java
@@ -1,6 +1,8 @@
 package com.riabi.springapp;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringAppApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(SpringAppApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringAppApplication.class, args);
