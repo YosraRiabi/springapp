f5f22fb9e0ecb4f66c7bf2aba8b86a70538fe2e0
YosraRiabi <riabiyosraa@gmail.com>
2019-01-25 13:45:03 +0100

    Added account register and profile mappings and templates

--- src/main/java/com/riabi/springapp/SpringAppApplication.java
+++ src/main/java/com/riabi/springapp/SpringAppApplication.java
@@ -13,6 +13,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 import sun.security.x509.X500Name;
 
 
@@ -31,6 +32,13 @@ public class SpringAppApplication {
     PrettyTime prettyTime(){
         return new PrettyTime();
    }
+
+    // TODO * Configuring this bean should not be needed once Spring Boot's Thymeleaf starter includes configuration
+    // TODO   for thymeleaf-extras-springsecurity5 (instead of thymeleaf-extras-springsecurity4)
+    @Bean
+    public SpringSecurityDialect securityDialect() {
+        return new SpringSecurityDialect();
+    }
 
 
 }
