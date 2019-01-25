3337479b0927824097c967c7ba863353418d0bb5
YosraRiabi <riabiyosraa@gmail.com>
2019-01-25 11:56:21 +0100

    H2 Console security configuration

--- src/main/java/com/riabi/springapp/security/SecurityConfiguration.java
+++ src/main/java/com/riabi/springapp/security/SecurityConfiguration.java
@@ -27,8 +27,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/actuator/").hasRole("ADMIN")
                 .antMatchers("/").permitAll()
                 .antMatchers("/link/submit").hasRole("USER")
+                .antMatchers("/h2-console/**").permitAll()
                 .and()
-                .formLogin();
+                .formLogin()
+                .and()
+                    .csrf().disable()
+                    .headers().frameOptions().disable();
     }
 
     @Override
