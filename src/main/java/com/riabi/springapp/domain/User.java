36e0a11ec2dcefae767d3beec7d4131fb8a4abdc
YosraRiabi <riabiyosraa@gmail.com>
2019-01-25 11:46:00 +0100

    Role&User

---
+++ src/main/java/com/riabi/springapp/domain/User.java
@@ -0,0 +1,117 @@
+package com.riabi.springapp.domain;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Entity
+public class User implements UserDetails {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @NonNull
+    @Size(min = 8, max = 20)
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NonNull
+    @Column(length = 100)
+    private String password;
+
+    @NonNull
+    @Column(nullable = false)
+    private boolean enabled;
+
+    public User() {
+    }
+
+    public User(@NonNull @Size(min = 8, max = 20) String email, @NonNull String password, @NonNull boolean enabled) {
+        this.email = email;
+        this.password = password;
+        this.enabled = enabled;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id",referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id",referencedColumnName = "id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+    public void addRoles(Set<Role> roles) {
+        roles.forEach(this::addRole);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true ;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
