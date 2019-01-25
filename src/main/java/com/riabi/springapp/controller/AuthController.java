3622e6ff5bb8ba2fadcbc8ecb1a4dcf64e906187
YosraRiabi <riabiyosraa@gmail.com>
2019-01-25 13:17:06 +0100

    Remember me & logout

---
+++ src/main/java/com/riabi/springapp/controller/AuthController.java
@@ -0,0 +1,13 @@
+package com.riabi.springapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "auth/login";
+    }
+}
