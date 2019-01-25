a2af3a356f7879b2fff7a0686da0be3ad2151507
YosraRiabi <riabiyosraa@gmail.com>
2019-01-25 11:45:44 +0100

    Role&User

---
+++ src/main/java/com/riabi/springapp/domain/Role.java
@@ -0,0 +1,56 @@
+package com.riabi.springapp.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Collection;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NonNull
+    private String name;
+
+
+
+    @ManyToMany( mappedBy = "roles")
+    private Collection<User> users;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Collection<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Collection<User> users) {
+        this.users = users;
+    }
+
+    public Role() {
+    }
+
+    public Role(@NonNull String name) {
+        this.name = name;
+    }
+}
